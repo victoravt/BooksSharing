18a60fbd35d8c5ed4befc5130bbb5a6566678adf
EPAM\Victor_Avtandilyan <victor_avtandilyan@epam.com>
2023-03-21 14:49:37 +0100

    Task 1; Initialized the base to start development. The Copilot generated secrets for dev, test, prod envs connection strings based on my written configuration string for dev env.

---
+++ BooksSharing.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BooksSharing.DataAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- BooksSharing/Program.cs
+++ BooksSharing/Program.cs
@@ -1,7 +1,13 @@
+using BooksSharing.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Connection string
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer("Data Source=.;Initial Catalog=BooksSharing;Integrated Security=True;"));
 
 var app = builder.Build();
 
@@ -12,6 +18,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
